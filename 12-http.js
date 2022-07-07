59623cf3db7cd8058bdbd7157bd66c5be216a814
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-07-07 17:17:39 +0530

    Building package.json

--- 12-http.js
+++ 12-http.js
@@ -1,5 +1,20 @@
-const { readFileSync, writeFileSync } = require("fs");
+const http = require("http");
 
-const httpFile = readFileSync("./app.js", "utf-8");
+//req -> Incoming Request
+//res -> Outgoing response
 
-writeFileSync("./12-http.js", httpFile);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Node Js is super cool. Hey created by Debasmit!!!");
+  }
+  if (req.url === "/about") {
+    res.end("Working on it");
+  }
+  res.end(`
+  <h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">Back Home</a>
+  `);
+});
+
+server.listen(5000);
--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const http = require("http");
+//npm - global command comes with node
+//npm --version
 
-//req -> Incoming Request
-//res -> Outgoing response
+//local dependency - use it only in this particular project
+// npm i <packageName>
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Node Js is super cool. Hey created by Debasmit!!!");
-  }
-  if (req.url === "/about") {
-    res.end("Working on it");
-  }
-  res.end(`
-  <h1>Oops!</h1>
-  <p>We can't seem to find the page you are looking for</p>
-  <a href="/">Back Home</a>
-  `);
-});
+//npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
 
-server.listen(5000);
+//package.json - manifest file (stores imp info abt project/package)
+//manual approach (create package.json in the root, create properties etc)
+//npm init
+//npm init -y (everything default)
+
+//package.json provides info abt the project as well as download the project dependencies
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
