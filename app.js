ba06b5bdd6af8a77a5730c8c9bd65042a5f6a94d
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-07-07 14:37:44 +0530

    Added File Sync

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
- 
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf8", (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+});
